74da1942fc47353267f6febb38a5656af4ebe074
vektorprime <vektorprime@GTX>
2025-03-22 12:18:12 -0500

    moved back to underscore name

--- target/debug/deps/ez_netflow_cli-7dfe396f7cfa2470.d
+++ target/debug/deps/ez_netflow_cli-7dfe396f7cfa2470.d
@@ -1,8 +1,9 @@
-C:\Users\vicha\rust\ez_netflow\target\debug\deps\libez_netflow_cli-7dfe396f7cfa2470.rmeta: ez-netflow-cli\src/main.rs Cargo.toml
+C:\Users\vicha\rust\ez_netflow\target\debug\deps\libez_netflow_cli-7dfe396f7cfa2470.rmeta: ez-netflow-cli\src/main.rs ez-netflow-cli\src\cli.rs Cargo.toml
 
-C:\Users\vicha\rust\ez_netflow\target\debug\deps\ez_netflow_cli-7dfe396f7cfa2470.d: ez-netflow-cli\src/main.rs Cargo.toml
+C:\Users\vicha\rust\ez_netflow\target\debug\deps\ez_netflow_cli-7dfe396f7cfa2470.d: ez-netflow-cli\src/main.rs ez-netflow-cli\src\cli.rs Cargo.toml
 
 ez-netflow-cli\src/main.rs:
+ez-netflow-cli\src\cli.rs:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=
